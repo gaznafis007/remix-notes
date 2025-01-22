7d1243dd2e1b697e81dd94a38a5c933b4ac641c2
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-22 20:18:37 +0600

    create the basic structure

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "@remix-run/react";
+
+const Navbar = () => {
+    return (
+        <nav className="flex flex-row justify-center space-x-6 text-xl py-4 font-semibold">
+            <NavLink to='/' className={({isActive}) => isActive ? 'text-white' : 'text-purple-500'}>Home</NavLink>
+            <NavLink to='/notes' className={({isActive}) => isActive ? 'text-white' : 'text-purple-500'}>Notes</NavLink>
+        </nav>
+    );
+};
+
+export default Navbar;
