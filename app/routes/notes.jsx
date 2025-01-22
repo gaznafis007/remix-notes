a24d5e61c4049d92aa90e317fd79c29f39834e06
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-22 20:01:02 +0600

    basic integration done

---
+++ app/routes/notes.jsx
@@ -0,0 +1,9 @@
+const NotesPage = () => {
+    return (
+        <div>
+            <h2 className="text-4xl text-center text-purple-400">Hello Remix</h2>
+        </div>
+    );
+};
+
+export default NotesPage;
